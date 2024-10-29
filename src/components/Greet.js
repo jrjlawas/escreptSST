71b7f1f98b8257b6f83a1927befdb900f5c23b93
jrjlawas <43806698+jrjlawas@users.noreply.github.com>
2024-10-29 20:45:21 +0800

    My First Commit Manual

---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Jay</h1>
+// }
+const Greet = () => <h1>Hello Jay</h1>
+
+export default Greet
---
+++ src/components/Hello.js
@@ -0,0 +1,7 @@
+import react from 'react'
+
+const Hello = () => {
+    return react.createElement('div',null,react.createElement('h1',null,'Jay'))
+}
+
+export default Hello

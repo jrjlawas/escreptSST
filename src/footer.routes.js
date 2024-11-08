c8232e8e9732c76ff0beb6b1e96da913c96d3406
jrjlawas <jayromellawas30@gmail.com>
2024-11-08 16:13:36 +0800

    updated footer

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -23,48 +23,35 @@ export default {
       icon: <FacebookIcon />,
       link: "https://www.facebook.com/profile.php?id=61562082383228",
     },
-    {
-      icon: <TwitterIcon />,
-      link: "https://twitter.com/creativetim",
-    },
-    {
-      icon: <YouTubeIcon />,
-      link: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
-    },
   ],
   menus: [
     {
       name: "company",
       items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
+        { name: "about us", href: "/pages/landing-pages/home" },
+        { name: "our team", href: "/pages/landing-pages/team" },
+        { name: "our approach", href: "/pages/landing-pages/approach" },
       ],
     },
     {
-      name: "resources",
+      name: "services",
       items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
+        { name: "it consultancy", href: "https://iradesign.io/" },
+        { name: "web development", href: "https://www.creative-tim.com/bits" },
+        { name: "BI Reporting and Support", href: "https://www.creative-tim.com/affiliates/new" },
       ],
     },
     {
       name: "help & support",
       items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
+        { name: "contact us", href: "https://www.creative-tim.com/contact-us" }
       ],
     },
     {
       name: "legal",
       items: [
         { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
-        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
-        { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
+        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" }
       ],
     },
   ],
